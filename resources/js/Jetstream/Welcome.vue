dd404c1089fe4c420861867958fd92e8bf14171e
Edgar Martinez <ecmartinez@users.noreply.github.com>
2021-11-14 00:57:41 -0600

    First Commit
    Jetstream with Inertia installed

---
+++ resources/js/Jetstream/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div>
+                <jet-application-logo class="block h-12 w-auto" />
+            </div>
+
+            <div class="mt-8 text-2xl">Mi nueva aplicacion</div>
+
+            <div class="mt-6 text-gray-500">Mi nueva aplicacion</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import JetApplicationLogo from "@/Jetstream/ApplicationLogo.vue";
+
+export default defineComponent({
+    components: {
+        JetApplicationLogo,
+    },
+});
+</script>
